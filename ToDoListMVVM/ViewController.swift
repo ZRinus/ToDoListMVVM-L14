55a1ef3bb48617ef5fd06fdb425c6d28f526d310
Rinat Zaripov <z_renat@inbox.ru>
2023-09-12 19:23:26 +0600

    Initial Commit

---
+++ ToDoListMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListMVVM
+//
+//  Created by Rinat Zaripov on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
